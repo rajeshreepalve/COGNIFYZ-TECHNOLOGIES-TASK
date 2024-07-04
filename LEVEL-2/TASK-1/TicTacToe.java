c87aeab93d3ac568dd0d37109f3a405eeda18be8
Rajeshree Palve <127953521+rajeshreepalve@users.noreply.github.com>
2024-07-04 15:39:26 +0530

    Add files via upload

---
+++ LEVEL-2/TASK-1/TicTacToe.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static final int SIZE = 3;
+    private static char[][] board = new char[SIZE][SIZE];
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        boolean playAgain = true;
+
+        while (playAgain) {
+            initializeBoard();
+            boolean gameWon = false;
+            boolean gameDraw = false;
+
+            while (!gameWon && !gameDraw) {
+                displayBoard();
+                playerMove(scanner);
+                gameWon = checkWin();
+                gameDraw = checkDraw();
+                if (!gameWon && !gameDraw) {
+                    switchPlayer();
+                }
+            }
+
+            displayBoard();
+            if (gameWon) {
+                System.out.println("Player " + currentPlayer + " wins!");
+            } else {
+                System.out.println("The game is a draw.");
+            }
+
+            System.out.print("Do you want to play again? (yes/no): ");
+            playAgain = scanner.next().equalsIgnoreCase("yes");
+        }
+
+        scanner.close();
+    }
+
+    private static void initializeBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    private static void displayBoard() {
+        System.out.println("Current board:");
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void playerMove(Scanner scanner) {
+        int row, col;
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row [1-3] and column [1-3]): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+            if (row >= 0 && row < SIZE && col >= 0 && col < SIZE && board[row][col] == '-') {
+                board[row][col] = currentPlayer;
+                break;
+            } else {
+                System.out.println("This move is not valid. Try again.");
+            }
+        }
+    }
+
+    private static boolean checkWin() {
+        // Check rows and columns
+        for (int i = 0; i < SIZE; i++) {
+            if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                return true;
+            }
+        }
+        // Check diagonals
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+            (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkDraw() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+}
